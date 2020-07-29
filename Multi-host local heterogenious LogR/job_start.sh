bea63d1bb20a4bd38e3dbb5325bfb1cc44bc4218
Eighonet <31664867+Eighonet@users.noreply.github.com>
2020-07-29 20:15:42 +0300

    Code design upd.

--- Multi-host local heterogenious LogR/job_start.sh
+++ Multi-host local heterogenious LogR/job_start.sh
@@ -2,7 +2,7 @@ python ../../../fate_flow/fate_flow_client.py -f upload -c upload_data_host_a1.j
 python ../../../fate_flow/fate_flow_client.py -f upload -c upload_data_host_a2.json
 python ../../../fate_flow/fate_flow_client.py -f upload -c upload_data_guest.json
 
-python ../../../fate_flow/fate_flow_client.py -f submit_job -c test_multi_host_job_conf.json -d test_hetero_lr_train_job_dsl.json
+python ../../../fate_flow/fate_flow_client.py -f submit_job -c hetero_lr_job_conf.json -d hetero_lr_job_dsl.json
 
 
 
